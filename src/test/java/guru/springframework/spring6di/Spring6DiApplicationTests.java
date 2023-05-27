ea13c0f20d9f1639cd145015851bda2b6d66d40a
Helen Ma <hhma@alumni.cpp.edu>
2023-05-27 16:07:06 -0400

    add test

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -3,7 +3,7 @@ package guru.springframework.spring6di;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //test
 class Spring6DiApplicationTests {
 
     @Test
